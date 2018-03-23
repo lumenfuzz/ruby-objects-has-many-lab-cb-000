ee8fd625bafa7b41c3579b8c0090f9b75441f7a5
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-23 22:10:09 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -9,13 +9,13 @@ class Author
 
   def add_post(post)
     @posts << post
-    post.author = @name
+    post.author = self
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
     @posts << post
-    post.author = @name
+    post.author = self
   end
 
   def self.post_count
