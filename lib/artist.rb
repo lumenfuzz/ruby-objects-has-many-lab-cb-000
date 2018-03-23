77957140dc121c0f50155a9c774004f4d517482c
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-23 21:34:19 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,5 @@
 class Artist
+  @@song_count = 0
   attr_reader :name, :songs
 
   def initialize(name)
@@ -9,8 +10,14 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = name
+    @@song_count += 1
   end
 
   def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = name
+    @@song_count += 1
   end
-end
+  
+end 
