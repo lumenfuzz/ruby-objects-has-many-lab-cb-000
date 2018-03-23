d37bfea5d092118ee6ee1970bfaaeb49e276249f
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-23 21:37:35 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,8 +1,14 @@
 class Author
+  attr_accessor :title, :author
 
   def initialize(name)
     @name = name
     @posts = []
+    @author = nil
   end
-  
+
+  def author_name
+    return @author
+  end
+
 end
